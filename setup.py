0055539327436f2da103823c638385d2c5764314
Vaibhav Parmar <62324376+vaibhav007-droid@users.noreply.github.com>
2021-03-18 10:31:36 +0530

    Update setup.py
    update setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+# from pip.req import parse_requirements
 import re, ast
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 # get version from __version__ variable in fedex_integration/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
