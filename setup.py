5021fdaa61140dd1bffceed6c0909630c0341c9f
Vaibhav Parmar <62324376+vaibhav007-droid@users.noreply.github.com>
2021-03-20 14:40:24 +0530

    Update And Work Properly
    Now The Error Was Gone And The App Installed Successfully..!!

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-# from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 import re, ast
 
-# Generator must be converted to list, or we will only have one chance to read each element, meaning that the first requirement will be skipped.
-requirements = list(requirements) 
-try:
-    requirements = [str(ir.req) for ir in install_reqs]
-except:
-    requirements = [str(ir.requirement) for ir in install_reqs]
-
 # get version from __version__ variable in fedex_integration/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+from fedex_integration import __version__ as version
 
-with open('fedex_integration/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
-requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='fedex_integration',
@@ -28,6 +19,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
